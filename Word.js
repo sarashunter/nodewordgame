354a91c5679b71a30b357b2c9aa09d90d350184f
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-09 20:11:34 -0400

    Add functions to Word.js

--- Word.js
+++ Word.js
@@ -3,8 +3,18 @@ var Letter = require('./Letter');
 var Word = function(wordToGuess){
     this.wordToGuess = wordToGuess;
     this.lettersArray = constructLettersArray(wordToGuess);
-
-    wordToGuess.split("").forEach
+    this.currentState = function(){
+        var currentString='';
+        this.lettersArray.forEach(function(element){
+            currentString + element.returnLetter();
+        })
+        return currentString;
+    };
+    this.checkLetter(guess){
+        this.lettersArray.forEach(function(element){
+            element.checkLetter();
+        })
+    }
 }
 
 var constructLettersArray = function(wordToGuess){
